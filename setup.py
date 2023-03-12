644b296c3723d7331b0be8122c0d197290524349
₿oss <110758135+vqea@users.noreply.github.com>
2023-03-11 21:26:09 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="discord_ios",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT License",
+    description="make your discord.py bots appear on mobile.",
+    keywords="discord.py, discord ios, discord ios status, discord bot mobile status, discord mobile bot status",
+    url="https://github.com/vqea/discord-ios",
+    author="vqea",
+    author_email="dqae.sucks@gmail.com",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
